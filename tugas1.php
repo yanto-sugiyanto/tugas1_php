94bd0bf0989af489a8acc553fff3f44a41e12e0e
yanto-sugiyanto <71365660+yanto-sugiyanto@users.noreply.github.com>
2020-10-07 12:32:46 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body><h1>Daftar Menu</h1>
+	<?php
+	$ayamgoreng ="Rp.13000";
+	$ayambakar = "Rp.15000";
+	$ayamsayur = "Rp.13000";
+	$tempegoreng ="Rp.1000";
+	$tahugoreng = "Rp.1000";
+	$soto = "Rp.3000";
+	$nasiputih ="Rp.5000";
+	$esteh ="Rp.4000";
+	$estebu ="Rp.5000";
+
+
+	echo"<ul>
+		<li> Ayam Goreng =".$ayamgoreng."</li>
+		<li> Ayam Bakar =".$ayambakar."</li>
+		<li> Ayam Sayur =".$ayamsayur."</li>
+		<li> Tempe Goreng =".$tempegoreng."</li>
+		<li> Tahu Goreng =".$tahugoreng."</li>
+		<li> soto =".$soto."</li>
+		<li> Nasi Putih =".$nasiputih."</li>
+		<li> Es Teh =".$esteh."</li>
+		<li> Es Tebu =".$estebu."</li>
+		</ul>";
+		?>
+
+</body>
+</html>
